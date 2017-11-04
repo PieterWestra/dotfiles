032055fb4acda01478306900541a13d4a25f3a98
Pieter <pietergwestra@hotmail.com>
2017-11-04 16:55:16 +0100

    Added OS and improved spacing

--- bin/info-get.sh
+++ bin/info-get.sh
@@ -5,6 +5,10 @@ new_line(){
 	echo
 }
 
+print_os(){
+	echo -e "${COLOR}Operating system:${NC}\t$(uname -o)"
+}
+
 print_distro(){
 	distroID=$(cat /etc/*release | grep DISTRIB_ID)
 	distroRL=$(cat /etc/*release | grep DISTRIB_RELEASE)
@@ -12,33 +16,33 @@ print_distro(){
 	
 	distro="${distroID##*=} ${distroRL##*=}"
 
-	echo -e "${COLOR}Distribution:${NC}\t$distro"
+	echo -e "${COLOR}Distribution:${NC}\t\t$distro"
 }
 
 print_kernel(){
-	echo -e "${COLOR}Kernel:${NC}\t\t$(uname -r)"
+	echo -e "${COLOR}Kernel version:${NC}\t\t$(uname -r)"
 }
 
 print_desktop(){
-	echo -e "${COLOR}Desktop:${NC}\t$DESKTOP_SESSION"
+	echo -e "${COLOR}Desktop:${NC}\t\t$DESKTOP_SESSION"
 }
 
 print_shell(){
-	echo -e "${COLOR}Shell:${NC}\t\t$SHELL"
+	echo -e "${COLOR}Shell:${NC}\t\t\t$SHELL"
 }
 
 print_user(){
-	echo -e "${COLOR}User:${NC}\t\t$USER"
+	echo -e "${COLOR}User:${NC}\t\t\t$USER"
 }
 
 print_dirs(){
-	echo -e "${COLOR}HOME:${NC}\t\t$HOME"
-	echo -e "${COLOR}Working dir:${NC}\t$PWD"
+	echo -e "${COLOR}HOME:${NC}\t\t\t$HOME"
+	echo -e "${COLOR}Working directory:${NC}\t$PWD"
 }
 
 print_cpu(){
 	cpu=$(cat /proc/cpuinfo | grep model\ name | head -n1)
-	echo -e "${COLOR}CPU:${NC}\t\t${cpu##*: }"
+	echo -e "${COLOR}CPU:${NC}\t\t\t${cpu##*: }"
 }
 
 print_ram(){
@@ -47,11 +51,12 @@ print_ram(){
 	ramTotal=${ramTotal##*:        } 
 	
 	# Convert kB to MB
-	echo -e "${COLOR}RAM:${NC}\t\t$(($ramTotal / 1000)) MB"
+	echo -e "${COLOR}RAM:${NC}\t\t\t$(($ramTotal / 1000)) MB"
 }
 
-print_distro
+print_os
 print_kernel
+print_distro
 print_desktop
 print_shell
 print_user
